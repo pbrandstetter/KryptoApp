04d9141bc12f6ca1c445f85e2b10a6e8f4df6786
Philipp Brandstetter <philipp.brandstetter57@gmail.com>
2019-10-09 17:09:48 +0200

    Added login

--- KryptoLibrary/Model/User.cs
+++ KryptoLibrary/Model/User.cs
@@ -1,16 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-
+﻿
 namespace KryptoApp.Model
 {
     public class User
     {
         public int Id { get; set; }
 
-        [Required]
         public string Username { get; set; }
 
-        [Required]
         public string Password { get; set; }
 
         public string PublicKey { get; set; }
